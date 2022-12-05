747bda893941be4e681d4d517d1a613b554aeedf
Ian Kollipara <ian.kollipara@cune.org>
2022-12-05 13:21:02 -0600

    rewrite

--- prisma/migrations/20221101083322_district_region/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `region` to the `District` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "District" ADD COLUMN     "region" "Region" NOT NULL;
--- src/server/trpc/router/subject.ts
+++
@@ -1,5 +0,0 @@
-import { protectedProcedure, router } from "../trpc";
-
-export const subjectRouter = router({
-  getAll: protectedProcedure.query(({ ctx }) => ctx.prisma.subject.findMany()),
-});
