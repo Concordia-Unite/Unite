28f44a08a18488550c6f21285d79353bd40a6bc3
Ian Kollipara <ian.kollipara@cune.org>
2022-12-07 14:25:24 -0600

    feat: Added Positions
    Valid Positions Enum was added.

---
+++ src/types/enums/position.ts
@@ -0,0 +1,9 @@
+export enum Position {
+  Deaconess = "Deaconess",
+  LutheranTeacher = "Lutheran Teacher",
+  DPM = "Director of Parish Music",
+  DCE = "Director of Christian Education",
+  DFLM = "Director of Family Life Ministries",
+  DCO = "Director of Christian Outreach",
+  DCM = "Director of Church Ministries",
+}
